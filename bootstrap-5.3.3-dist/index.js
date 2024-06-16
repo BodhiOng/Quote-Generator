a63ce59b0629aa8848f279e808f91c7c55c404da
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-06-17 03:30:29 +0800

    Made text overlay on top of image display

--- bootstrap-5.3.3-dist/index.js
+++ bootstrap-5.3.3-dist/index.js
@@ -14,3 +14,21 @@ document.addEventListener("DOMContentLoaded", function() {
     var screenWidth = window.innerWidth;
     img.src = "https://picsum.photos/" + screenWidth + "/" + imgHeight;
 });
+
+// $(document).ready(function() {
+//     var category = 'happiness';
+//     $.ajax({
+//         method: 'GET',
+//         url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+//         headers: { 'X-Api-Key': '4S1xFyCAA+WxkypINY9B9w==8eoKCeoAbuHKsAHz'},
+//         contentType: 'application/json',
+//         success: function(result) {
+//             if(result.length > 0) {
+//                 $('#quoteText').text(result[0].quote + ' - ' + result[0].author);
+//             }
+//         },
+//         error: function ajaxError(jqXHR) {
+//             console.error('Error: ', jqXHR.responseText);
+//         }
+//     });
+// });
