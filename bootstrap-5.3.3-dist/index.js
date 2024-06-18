ffbc4901f50de44fa27b3a75043495a94df49be1
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-06-19 01:57:36 +0800

    Responsive on all axis, image generated per screen resizing

--- bootstrap-5.3.3-dist/index.js
+++ bootstrap-5.3.3-dist/index.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     var img = document.getElementById("dynamicImage");
-    var header = document.querySelector('.header');
-    var footer = document.querySelector('.footer');
-    
-    var screenHeight = window.innerHeight;
-    var headerHeight = header.offsetHeight;
-    var footerHeight = footer.offsetHeight;
 
-    var imgHeight = screenHeight - headerHeight - footerHeight;
-    
-    img.style.height = imgHeight + "px";
-    
-    var screenWidth = window.innerWidth;
-    img.src = "https://picsum.photos/" + screenWidth + "/" + imgHeight;
+    function setImageHeight() {
+        var screenHeight = window.innerHeight;
+        var headerHeight = document.querySelector('.header').offsetHeight;
+        var footerHeight = document.querySelector('.footer').offsetHeight;
+
+        var imgHeight = screenHeight - headerHeight - footerHeight;
+        img.style.height = imgHeight + "px";
+
+        var screenWidth = window.innerWidth;
+        var newImgSrc = "https://picsum.photos/" + screenWidth + "/" + imgHeight;
+        
+        preloadImage(newImgSrc, function() {
+            img.src = newImgSrc;
+            img.classList.add('loaded');
+        });
+    }
+
+    function preloadImage(src, callback) {
+        var preImg = new Image();
+        preImg.onload = callback;
+        preImg.src = src;
+    }
+
+    setImageHeight();
+    window.addEventListener('resize', setImageHeight);
 });
+
 
 // $(document).ready(function() {
 //     var category = 'happiness';
